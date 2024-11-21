6af434212d08b8f8f94d3cb519b876819b3f25ab
Gabriele Nardella <gabrynardy5@gmail.com>
2024-11-21 16:03:57 +0100

    project init

---
+++ src/main/java/uib/swarchitecture/quepasa/QuepasaApplication.java
@@ -0,0 +1,13 @@
+package uib.swarchitecture.quepasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuepasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuepasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uib/swarchitecture/quepasa/QuepasaApplicationTests.java
@@ -0,0 +1,13 @@
+package uib.swarchitecture.quepasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuepasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
