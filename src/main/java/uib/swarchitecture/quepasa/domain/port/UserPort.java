5e8031a5478cd01cffdab69deff532aefd066fa6
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2024-12-17 03:55:52 +0100

    Define port methods

---
+++ src/main/java/uib/swarchitecture/quepasa/domain/port/UserPort.java
@@ -0,0 +1,12 @@
+package uib.swarchitecture.quepasa.domain.port;
+
+import uib.swarchitecture.quepasa.domain.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserPort {
+    public boolean existsByUsername(String username);
+    public boolean existsByEmail(String email);
+    public User addUser(User user);
+}
