762a0e1eaa930c2bbade55b4b0c3f292c586b684
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2024-12-17 03:57:24 +0100

    Implement service to add a user

--- src/main/java/uib/swarchitecture/quepasa/domain/service/UserService.java
+++ src/main/java/uib/swarchitecture/quepasa/domain/service/UserService.java
@@ -1,4 +1,35 @@
 package uib.swarchitecture.quepasa.domain.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uib.swarchitecture.quepasa.domain.exceptions.EmailAlreadyExistsException;
+import uib.swarchitecture.quepasa.domain.exceptions.UsernameAlreadyExistsException;
+import uib.swarchitecture.quepasa.domain.model.User;
+import uib.swarchitecture.quepasa.domain.port.UserPort;
+
+@Component
 public class UserService {
+
+    private UserPort userPort;
+
+    @Autowired
+    public UserService(UserPort userPort) {
+        this.userPort = userPort;
+    }
+
+    public User addUser(User user) {
+        if (userPort.existsByEmail(user.getEmail())) {
+            throw new EmailAlreadyExistsException(user.getEmail());
+        }
+
+        if (userPort.existsByUsername(user.getUsername())) {
+            throw new UsernameAlreadyExistsException(user.getUsername());
+        }
+
+        try {
+            return userPort.addUser(user);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create user", e);
+        }
+    }
 }
