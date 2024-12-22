d377fdbb98454eea18b28da3918ffe185d8a8c63
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2024-12-22 21:24:52 +0100

    Add Builder pattern and Comparable implementation to Chat, Message, and UserChat models

--- src/main/java/uib/swarchitecture/quepasa/domain/models/Message.java
+++ src/main/java/uib/swarchitecture/quepasa/domain/models/Message.java
@@ -1,5 +1,6 @@
 package uib.swarchitecture.quepasa.domain.models;
 
+import lombok.Builder;
 import uib.swarchitecture.quepasa.domain.models.enums.MessageType;
 
 import java.time.LocalDateTime;
@@ -11,10 +12,16 @@ import lombok.AllArgsConstructor;
 @Data // Genera getters, setters, toString, equals, hashCode
 @NoArgsConstructor // Genera un constructor sin argumentos
 @AllArgsConstructor // Genera un constructor con todos los campos
-public class Message {
+@Builder // Genera el patrón builder
+public class Message implements Comparable<Message> {
     private long id;
     private String content;
     private LocalDateTime timestamp;
     private MessageType type;
     private User author;
+
+    @Override
+    public int compareTo(Message o) {
+        return o.getTimestamp().compareTo(this.timestamp);
+    }
 }
